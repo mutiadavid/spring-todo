e84e38f0ec5ea8605eba9409810e86203891dbd4
david mutia <davidmutia47@gmail.com>
2019-07-05 14:35:21 +0300

    Delete one HomeController.

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,15 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.example.demo.controllers;
 
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RequestMapping(path = "api/home")
-@RestController
+@Controller
 public class HomeController {
-    @RequestMapping(value = "/Hello",method = RequestMethod.GET)
-    public String hello(){
-        return  "Hello Spring!";
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public RedirectView main() {
+        return new RedirectView("swagger-ui.html");
     }
+
 }
